f2232b43c2993a1aafea49b9860e4abe5e5be151
nkpoid <nkpoidjp@gmail.com>
2021-07-09 15:34:57 +0900

    add content-type

--- main.go
+++ main.go
@@ -101,6 +101,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res) //nolint:errcheck
 }
